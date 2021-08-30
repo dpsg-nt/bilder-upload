f64649bfbabcc7cce9d61a73692065022e95a685
Jakob Jarosch <dev@jakobjarosch.de>
2021-08-31 00:27:15 +0200

    Add sentry integration

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,13 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import * as React from "react";
 import ReactDOM from "react-dom";
 
+import * as Sentry from "@sentry/react";
+
 import { App } from "./App";
 import { theme } from "./theme/theme";
+
+if (window.location.host !== "localhost")
+  Sentry.init({ dsn: "https://585fc4a63fbf4125b6dd9c5bb1bbce3a@o982758.ingest.sentry.io/5938207" });
 
 ReactDOM.render(
   <React.StrictMode>
